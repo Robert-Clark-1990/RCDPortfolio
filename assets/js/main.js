3a27cc6f205528414456af9a6b674412268ae469
Robert-Clark-1990 <robert.elliott.clark@outlook.com>
2021-07-20 20:47:16 +0000

    fix JS issues with scroll, add new img to contact, and other small fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,3 @@
-const swiper = new Swiper('.swiper-container', {
-    // Optional parameters
-    direction: 'vertical',
-    loop: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: {
-        delay: 2000,
-        disableOnInteraction: false,
-      },
-    },
-  });
-  
+
+
+ 
